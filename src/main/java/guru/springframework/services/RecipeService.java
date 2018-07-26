ad41365786889aeb65abbdb1dfe7af3c67b587db
SancraianR <radu.sancraian@paddypowerbetfair.com>
2018-07-26 13:46:59 +0300

    Add Command Object.

--- src/main/java/guru/springframework/services/RecipeService.java
+++ src/main/java/guru/springframework/services/RecipeService.java
@@ -1,5 +1,6 @@
 package guru.springframework.services;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.domain.Recipe;
 
 import java.util.Set;
@@ -11,4 +12,6 @@ public interface RecipeService {
 
     Set<Recipe> getRecipes();
     Recipe findById(Long id);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
 }
