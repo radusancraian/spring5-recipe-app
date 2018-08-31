284197ff05474435b5106e2b817d4f20ca3c6986
radusancraian <radu_sancraian@yahoo.com>
2018-09-01 02:08:30 +0300

    Delete ingredients functionality.

--- src/test/java/guru/springframework/controllers/IngredientControllerTest.java
+++ src/test/java/guru/springframework/controllers/IngredientControllerTest.java
@@ -80,4 +80,14 @@ public class IngredientControllerTest {
         verify(ingredientService, times(1)).findByRecipeIdAndIngredientId(anyLong(), anyLong());
     }
 
+    @Test
+    public void testDeleteAction() throws Exception {
+
+        mockMvc.perform(get("/recipe/2/ingredient/10/delete"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/recipe/2/ingredients"));
+
+        verify(ingredientService, times(1)).deleteById(anyLong());
+
+    }
 }
