37c37bdd3df72cf279697f81b36b8ff51f0a9ec9
radu.sancraian <radu.sancraian@paddypowerbetfair.com>
2018-09-21 17:25:00 +0300

    Code refactoring.

--- src/main/java/guru/springframework/controllers/ImageController.java
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -27,6 +28,7 @@ public class ImageController {
         return "recipe/imageuploadform";
     }
 
+    @PostMapping("/recipe/{id}/image")
     public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file) {
 
         imageService.saveImageFile(Long.valueOf(id), file);
